58dfbddb41d1add605fcec3ec2062e250c5a5612
souravjavapro <sourav02062000@gmail.com>
2024-12-12 11:58:56 +0530

    commit program.cs

--- TaskManagementAPI/Program.cs
+++ TaskManagementAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using TaskManagementAPI.Data;
 using TaskManagementAPI.Mapping;
 using TaskManagementAPI.Services;
@@ -15,8 +16,14 @@ builder.Services.AddScoped<ITaskService, TaskService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var logger = new LoggerConfiguration()
+    .WriteTo.File("Logs/CollegeAdmissions_Logs.txt", rollingInterval: RollingInterval.Day)
+    .MinimumLevel.Error()
+    .CreateLogger();
+
 builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+builder.Logging.AddSerilog(logger);
+
 
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
